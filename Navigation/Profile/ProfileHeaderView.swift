2f34d04bab17a58f1145fe23160bbb0d92a604ff
Dmitriy Lobanov <dvmald@icloud.com>
2021-08-28 22:42:14 +0300

    - выполнены пункты 1-4

---
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Dmitrii Lobanov on 28.08.2021.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+
+  
+
+}
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -11,7 +11,7 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        view.backgroundColor = .lightGray
         self.title = "Profile"
     }
     
